d61fec80cf1509fe881f3d5dd5ec89cc73b2c072
phani2585 <phani2585@gmail.com>
2019-04-07 01:20:37 +0530

    part_A final commit

--- stub/js/index.js
+++ stub/js/index.js
@@ -1,6 +1,6 @@
 
 // Get the signin modal
-var signinmodal = document.getElementById('sign-in-modal');
+var signinmodal = document.getElementById('modelSignIn');
 
 // Get the button that opens the modal
 var signinbtn = document.getElementById("sign-in-btn");
@@ -8,30 +8,29 @@ var signinbtn = document.getElementById("sign-in-btn");
 // Get the <span> element that closes the modal
 var signinspan = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
+// When the user clicks the button, open the modal 
 signinbtn.onclick = function() {
-signinmodal.style.display = "block";
+  signinmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 signinspan.onclick = function() {
-signinmodal.style.display = "none";
+  signinmodal.style.display = "none";
 }
 
-//When the user clicks on Sign Up hyperlink in the Signin Modal
 SignUp.onclick = function() {
-signupmodal.style.display = "block";
-signinmodal.style.display = "none";
+  signupmodal.style.display = "block";
+  signinmodal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == signinmodal){
-signinmodal.style.display = "none";}
-}
+window.addEventListener('click' , function(event) {
+  if (event.target == signinmodal) {
+    signinmodal.style.display = "none";
+  }
+});
 
 // Get the signup modal
-var signupmodal = document.getElementById('sign-up-modal');
+var signupmodal = document.getElementById('modelSignUp');
 
 // Get the button that opens the modal
 var signupbtn = document.getElementById("sign-up-btn");
@@ -41,29 +40,24 @@ var signupspan = document.getElementsByClassName("close")[1];
 
 // When the user clicks the button, open the modal 
 signupbtn.onclick = function() {
-signupmodal.style.display = "block";
+  signupmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 signupspan.onclick = function() {
-signupmodal.style.display = "none";
+  signupmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener('click' , function(event) {
-if (event.target == signupmodal) {
-signupmodal.style.display = "none";
+  if (event.target == signupmodal) {
+    signupmodal.style.display = "none";
   }
 });
 
-/******************************create post******************************* */
 
 // Get the create post modal
-//var createpostmodal = document.getElementById('create-modal-Post');
-
-// Get the create post modal
-var createpostmodal = document.getElementById('create-post-modal');
-
+var createpostmodal = document.getElementById('modelCreatePost');
 
 // Get the button that opens the modal
 var createpostbtn = document.getElementById("create-post-btn");
@@ -73,34 +67,27 @@ var createpostspan = document.getElementsByClassName("close")[2];
 
 //When the user clicks on CreatePost, open the modal
 createpostbtn.onclick = function() {
-createpostmodal.style.display = "block";
+  createpostmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 createpostspan.onclick = function() {
-createpostmodal.style.display = "none";
+  createpostmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener('click' , function(event) {
-if (event.target == createpostmodal) {
-  createpostmodal.style.display = "none";
+  if (event.target == createpostmodal) {
+    createpostmodal.style.display = "none";
   }
 });
 
+
 //When the user clicks on AllPosts, redirect to bloglist.html
-
-var allposts = document.getElementById("all-posts-btn");
-allposts.onclick = function() {
-window.location.href = "html/bloglist.html";
-}
-
 var allpostsbtn = document.getElementById("all-posts-btn");
 allpostsbtn.onclick = function() {
-window.location.href = "html/bloglist.html";
+  window.location.href = "html/bloglist.html";
 }
 
 //When the user clicks on Sign Up hyperlink in the Signin Modal, redirect to Sign Up Modal
 //code to be added 
-
-
